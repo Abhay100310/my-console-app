ea44041afa5f81ec5f1f5537280795ee6eb67355
Abhay100310 <78473850+Abhay100310@users.noreply.github.com>
2021-03-23 20:27:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+    var display = "my first console project"
+    console.log(display);
+
+    var full_name = "Abhay Vishwakarma"
+    console.log(full_name);
+    
+    var max_num = (2,3,4,5,1,7,90).math.max
+    console.log(max_num);
+
